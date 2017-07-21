dd07bfeff5348ce8ba0c480b2172588bf8f5b318
Dan Neame <danneame@gmail.com>
2017-07-21 16:57:09 +0100

    username and password parsers

--- api/parsers/index.js
+++ api/parsers/index.js
@@ -6,4 +6,20 @@ const positiveInteger = (str) => {
   return int;
 };
 
+const username = (str) => {
+  if (str && str.length && /^[a-z0-9_-]{3,20}$/i.test(str)) {
+    return str;
+  }
+  return false;
+};
+
+const password = (str) => {
+  if (str && str.length > 5) {
+    return str;
+  }
+  return false;
+};
+
 exports.positiveInteger = positiveInteger;
+exports.username = username;
+exports.password = password;
--- api/test/spec/parsers/index.js
+++ api/test/spec/parsers/index.js
@@ -19,4 +19,48 @@ describe('parsers', () => {
       assert.ok(parsers.positiveInteger('3'));
     });
   });
+
+  describe('username', () => {
+    it('returns false if arg is undefined', () => {
+      assert.equal(parsers.username(), false);
+    });
+
+    it('returns false if arg is empty', () => {
+      assert.equal(parsers.username(''), false);
+    });
+
+    it('returns false if arg is less than 3 characters long', () => {
+      assert.equal(parsers.username('hi'), false);
+    });
+
+    it('returns false when the arg is over 20 characters long', () => {
+      assert.equal(parsers.username('0.2'), false);
+    });
+
+    it('returns false if the arg contains anything other than letters, digits, underscores and hyphens', () => {
+      assert.equal(parsers.username('spaces are not allowed'), false);
+    });
+
+    it('returns true if the arg contains valid characters and is within the length range', () => {
+      assert.ok(parsers.username('valid_123'));
+    });
+  });
+
+  describe('password', () => {
+    it('returns false if arg is undefined', () => {
+      assert.equal(parsers.password(), false);
+    });
+
+    it('returns false if arg is empty', () => {
+      assert.equal(parsers.password(''), false);
+    });
+
+    it('returns false if arg is less than 6 characters long', () => {
+      assert.equal(parsers.password('five5'), false);
+    });
+
+    it('returns true if arg is at least 6 characters long', () => {
+      assert.ok(parsers.password('sixsix'));
+    });
+  });
 });
